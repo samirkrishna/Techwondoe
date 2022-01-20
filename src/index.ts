5d91b2514a482065260e1c524375631284143608
Samir Krishna Cholleti <samirkrishna.cholleti@infor.com>
2022-01-20 19:59:43 +0530

    Connected to Postgres DB

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,24 @@ import companyRoutes from './routes/companyRoutes'
 import teamRoutes from './routes/teamRoutes'
 import { error } from './routes/error'
 
+// const Pool = require('pg').Pool
+
+import Pool from 'pg';
+
 const app: Application = express();
 
 const PORT: Number = 8000;
+
+export const pool = new Pool.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'password',
+    port: 5432,
+})
+console.log("inside index");
+
+
 
 app.use(express.json())
 app.use("/api/v1/company", companyRoutes);
